14ef2dca8e9535333ff45ccd8da408b41274f2ce
justMaxIt <maximfedor2015@gmail.com>
2021-02-11 23:42:17 +0200

    working js code

--- index.js
+++ index.js
@@ -7,4 +7,36 @@ const model = [
   },
 ];
 
-document.querySelector("#site");
+const $site = document.querySelector("#site");
+
+model.forEach((block) => {
+  let html = "";
+
+  if (block.type === "title") {
+    html = `
+            <div class="row">
+                <div class="col-sm">
+                    <h1>${block.value}</h1>
+                </div>
+            </div>
+    `;
+  } else if (block.type === "text") {
+    html = `
+            <div class="row">
+               <div class="col-sm">
+            <p>${block.value}</p>
+               </div>
+            </div>
+            `;
+  } else if (block.type === "columns") {
+    // html = `
+    //         <div class="row">
+    //            <div class="col-sm">
+    //         <p>${block.value}</p>
+    //            </div>
+    //         </div>
+    //         `;
+  }
+  console.log(html);
+  $site.insertAdjacentHTML("beforeend", html);
+});
