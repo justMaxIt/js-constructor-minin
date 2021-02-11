1eeea8272b417f014c50983aed7983bc0eaa1498
justMaxIt <maximfedor2015@gmail.com>
2021-02-11 15:31:02 +0200

    add index.js & main.css

---
+++ index.js
@@ -0,0 +1,10 @@
+const model = [
+  { type: "title", value: "Hello world from JS" },
+  { type: "text", value: "some text" },
+  {
+    type: "columns",
+    value: ["111111", "222222", "3333333"],
+  },
+];
+
+document.querySelector("#site");
